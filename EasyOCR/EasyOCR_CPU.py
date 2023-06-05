e8dc6d36b5e2b4ac8868a78582721490fe0b677c
Niklas Sjöstedt <77779163+NiklasSjostedt@users.noreply.github.com>
2023-06-05 14:39:03 +0200

    Add files via upload

---
+++ EasyOCR/EasyOCR_CPU.py
@@ -0,0 +1,140 @@
+import os
+import re
+import time
+import json
+import psutil
+import pandas as pd
+import numpy as np
+import subprocess
+import threading
+from PIL import Image
+import easyocr
+import Levenshtein as lev
+
+# Constants
+BASE_DIR = r'C:\Users\nikla\Desktop\Examensarbete\OCR_Comparison\SciTSR\2images'
+IMAGE_DIR = os.path.join(BASE_DIR, 'images')
+JSON_DIR = os.path.join(BASE_DIR, 'structure')
+PATTERN = re.compile(r'[-_\s]')
+
+
+class ResourceMonitor:
+    """Monitors the CPU and GPU resource usage."""
+
+    def __init__(self, interval=0.5):
+        self.interval = interval
+        self.process = psutil.Process(os.getpid())
+        self.cpu_percentages = []
+        self.gpu_percentages = []
+        self.stop_monitoring = False
+        self.logical_cores = psutil.cpu_count()
+
+    def monitor(self):
+        """Monitor CPU and GPU usage in an infinite loop."""
+        while not self.stop_monitoring:
+            cpu_percent = self.process.cpu_percent(
+                interval=self.interval) / self.logical_cores
+            self.cpu_percentages.append(cpu_percent)
+            try:
+                output = subprocess.check_output(['nvidia-smi', '--query-gpu=utilization.gpu',
+                                                  '--format=csv,noheader,nounits'])
+                gpu_percent = float(output.decode('utf-8').strip())
+            except (subprocess.CalledProcessError, FileNotFoundError):
+                gpu_percent = -1
+            self.gpu_percentages.append(gpu_percent)
+
+    def start(self):
+        """Start monitoring in a separate thread."""
+        self.monitoring_thread = threading.Thread(target=self.monitor)
+        self.monitoring_thread.start()
+
+    def stop(self):
+        """Stop monitoring and calculate the average CPU and GPU usage."""
+        self.stop_monitoring = True
+        self.monitoring_thread.join()
+        avg_cpu_percent = sum(self.cpu_percentages) / len(self.cpu_percentages)
+        avg_gpu_percent = sum(self.gpu_percentages) / len(self.gpu_percentages)
+        return avg_cpu_percent, avg_gpu_percent
+
+
+def create_dataframe_and_save(ocr_texts, json_texts, processing_times, sorted_text=False):
+    """Creates a pandas dataframe from the provided lists and saves it to an Excel file."""
+    DistanceEditDict = {}
+    file_name = 'EasyOCR_Refactored.xlsx' if sorted_text else 'EasyOCR_Refactored_No_SORT.xlsx'
+    for i, filename in enumerate(os.listdir(IMAGE_DIR)):
+        DistanceEdit = lev.distance(ocr_texts[i], json_texts[i])
+        percentage = (max(len(ocr_texts[i]), len(json_texts[i])) - DistanceEdit) / max(len(
+            ocr_texts[i]), len(json_texts[i]))
+        filename = os.path.splitext(filename)[0]
+        chars_per_millisecond = len(
+            ocr_texts[i]) / (processing_times[i] * 1000)
+        DistanceEditDict[filename] = {'Distance Edit': DistanceEdit, 'Percentage': percentage,
+                                      'CharsPerMillisecond': chars_per_millisecond, 'Time': processing_times[i]}
+    df = pd.DataFrame.from_dict(DistanceEditDict, orient='index')
+    df.index.name = 'FileName'
+    df.to_excel(file_name)
+
+
+def process_files(image_dir, json_dir):
+    """Process image and JSON files from specified directories."""
+    reader = easyocr.Reader(['en'], gpu=False)
+    processing_times = []
+    ocr_texts = []
+    json_texts = []
+    sorted_ocr_texts = []
+    sorted_json_texts = []
+
+    for filename in os.listdir(image_dir):
+        if filename.endswith('.png'):
+            full_filename = os.path.join(image_dir, filename)
+            img = Image.open(full_filename).convert('RGB')
+            img_array = np.array(img)
+            start_time = time.time()
+            result = reader.readtext(img_array)
+            end_time = time.time()
+            processing_times.append(end_time - start_time)
+            ocr_text = ' '.join(t[1] for t in result).split()
+            join_ocr_text = ''.join(ocr_text)
+            re_ocr_text = re.sub(PATTERN, '', join_ocr_text)
+            sorted_ocr_text = ''.join(sorted(ocr_text))
+            sorted_ocr_text_join = re.sub(PATTERN, '', sorted_ocr_text)
+            ocr_texts.append(re_ocr_text)
+            sorted_ocr_texts.append(''.join(sorted(sorted_ocr_text_join)))
+
+    for json_filename in os.listdir(json_dir):
+        full_filename = os.path.join(json_dir, json_filename)
+        with open(full_filename, 'r') as f:
+            data = json.load(f)
+        json_text = []
+        for cell in data['cells']:
+            json_text.extend(cell['content'])
+        join_json_text = ''.join(json_text)
+        sorted_json_text = ''.join(sorted(join_json_text))
+        json_texts.append(''.join(json_text))
+        sorted_json_texts.append(sorted_json_text)
+
+    return ocr_texts, json_texts, sorted_ocr_texts, sorted_json_texts, processing_times
+
+
+if __name__ == "__main__":
+    resource_monitor = ResourceMonitor(interval=0.5)
+    resource_monitor.start()
+
+    start_time = time.time()
+    ocr_texts, json_texts, sorted_ocr_texts, sorted_json_texts, processing_times = process_files(
+        IMAGE_DIR, JSON_DIR)
+    create_dataframe_and_save(ocr_texts, json_texts,
+                              processing_times, sorted_text=False)
+    create_dataframe_and_save(
+        sorted_ocr_texts, sorted_json_texts, processing_times, sorted_text=True)
+    end_time = time.time()
+
+    avg_cpu_percent, avg_gpu_percent = resource_monitor.stop()
+
+    rss = psutil.Process(os.getpid()).memory_info().rss / (1024 * 1024)
+
+    print(f"Execution time: {end_time - start_time:.2f} seconds")
+    print(f"Average CPU usage: {avg_cpu_percent:.2f}%")
+    print(f"Average GPU usage: {avg_gpu_percent:.2f}%")
+    print(f"RAM usage: {rss:.2f} MB")
+
